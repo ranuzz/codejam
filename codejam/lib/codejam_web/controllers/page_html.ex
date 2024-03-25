40a19f9a2be804ce6d1207c02b02f255e6f0422c
Shekhar Chandra <ranuzz@outlook.com>
2024-03-25 22:59:18 +0530

    add new landing copy

--- codejam/lib/codejam_web/controllers/page_html.ex
+++ codejam/lib/codejam_web/controllers/page_html.ex
@@ -6,21 +6,11 @@ defmodule CodejamWeb.PageHTML do
     <.hero_title_landing>
       <div class="flex flex-col">
         <.title>
-          Real-Time Collaboration Platform for <br />Understanding Code Repositories.
+          Codejam, the collaborative <br /> workspace for developers.
         </.title>
         <.sub_title>
-          <ul>
-            <li>Debugging on demand: Get instant help from teammates, live in your code editor.</li>
-            <li>
-              Onboard like a pro: Collaborative exploration empowers new engineers to hit the ground running.
-            </li>
-            <li>
-              Docs built from the source: Generate living documentation directly from your code discussions.
-            </li>
-            <li>
-              Seamless bug reporting: File tickets right from your code, with all context captured.
-            </li>
-          </ul>
+          Boost your team's coding productivity with Codejam.<br />
+          Discuss, plan, debug, and annotate code together in real-time, keeping your projects on track.
         </.sub_title>
         <div class="mt-10">
           <.link_button href={~p"/users/register"} class="accent">Get Started !</.link_button>
@@ -37,6 +27,51 @@ defmodule CodejamWeb.PageHTML do
         </div>
       </div>
     </.hero_title_landing>
+    <section class="container mx-auto py-12">
+      <div class="flex flex-col items-center p-6">
+        <.title>Key Features</.title>
+      </div>
+    </section>
+    <section class="container mx-auto py-12">
+      <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
+        <div class="flex flex-col items-center p-6 rounded-lg shadow-md hover:shadow-lg hover:bg-gray-100">
+          <span class="material-symbols-outlined">
+            description
+          </span>
+          <h3 class="text-lg font-semibold mb-2">Streamlined documentation</h3>
+          <p class="text-gray-600 text-center">
+            Add and update comments collaboratively, ensuring everyone has access to the latest information.
+          </p>
+        </div>
+        <div class="flex flex-col items-center p-6 rounded-lg shadow-md hover:shadow-lg hover:bg-gray-100">
+          <span class="material-symbols-outlined">
+            account_box
+          </span>
+          <h3 class="text-lg font-semibold mb-2">Seamless onboarding</h3>
+          <p class="text-gray-600 text-center">
+            Create interactive walkthroughs and tutorials directly within your codebase, empowering new team members to get up to speed quickly.
+          </p>
+        </div>
+        <div class="flex flex-col items-center p-6 rounded-lg shadow-md hover:shadow-lg hover:bg-gray-100">
+          <span class="material-symbols-outlined">
+            description
+          </span>
+          <h3 class="text-lg font-semibold mb-2">Actionable code reviews</h3>
+          <p class="text-gray-600 text-center">
+            Make TODOs, FIXMEs, and NOTES actionable by assigning owners and setting reminders, keeping tasks organized and accountable.
+          </p>
+        </div>
+        <div class="flex flex-col items-center p-6 rounded-lg shadow-md hover:shadow-lg hover:bg-gray-100">
+          <span class="material-symbols-outlined">
+            description
+          </span>
+          <h3 class="text-lg font-semibold mb-2">Real-time collaboration</h3>
+          <p class="text-gray-600 text-center">
+            Debug faster with instant feedback and code reviews, fostering a dynamic and efficient development environment.
+          </p>
+        </div>
+      </div>
+    </section>
     """
   end
 end
